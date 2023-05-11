d0e00307675e8c4733a056fa8dea533ffd23dd16
Mohan <cutemohan655@gmail.com>
2023-05-11 12:12:17 +0530

    implemantation 1.0

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p = new Product();
+
+        p.product(2, 2);
+        p.product(2, 2, 2);
+        p.product(2.0, 2.0);
+    }
+
+    static class Product{
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return this.product(x,y) * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
+
